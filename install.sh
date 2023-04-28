f0e39eaf9a3fc65228c859e27fe330753cba4678
kplr-io <mehdi.lamrani@kplr.fr>
2023-04-28 14:31:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearm
 sudo apt-get install apt-transport-https
 echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
 sudo apt-get update && sudo apt-get install elasticsearch
+sudo apt-get install kibana
